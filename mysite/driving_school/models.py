6424c4de2d79e49fceff227f19f474fa87724267
Stephane <stephane1.ngo@epitech.eu>
2019-06-11 16:52:49 +0200

    add admin table databases

--- mysite/driving_school/models.py
+++ mysite/driving_school/models.py
@@ -11,14 +11,20 @@ class User(models.Model):
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
     role = models.CharField(max_length=25, choices=ROLE_CHOICE)
+    def __str__(self):
+        return self.username
 
 class Planning(models.Model):
     date = models.DateField()
     location = models.CharField(max_length=50)
     owner = models.ForeignKey(User,to_field='id', on_delete=models.CASCADE, related_name='owner')
     student = models.ForeignKey(User, to_field='id', on_delete=models.CASCADE, related_name='user')
+    def __str__(self):
+        return self.date
 
 class UserPlan(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     payed_hour = models.IntegerField(default=0)
     lessons = models.IntegerField(default=0)
+    def __str__(self):
+        return self.owner
