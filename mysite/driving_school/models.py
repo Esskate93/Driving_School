9d8fdff5b61ce9ddd2514c4be1f3d3f5ba21f690
Stephane <stephane1.ngo@epitech.eu>
2019-06-11 16:38:49 +0200

    first

---
+++ mysite/driving_school/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+ROLE_CHOICE = (
+    ("secretary", "secretary"),
+    ("instructor", "instructor"),
+    ("students", "students")
+)
+
+# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    role = models.CharField(max_length=25, choices=ROLE_CHOICE)
+
+class Planning(models.Model):
+    date = models.DateField()
+    location = models.CharField(max_length=50)
+    owner = models.ForeignKey(User,to_field='id', on_delete=models.CASCADE, related_name='owner')
+    student = models.ForeignKey(User, to_field='id', on_delete=models.CASCADE, related_name='user')
+
+class UserPlan(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    payed_hour = models.IntegerField(default=0)
+    lessons = models.IntegerField(default=0)
