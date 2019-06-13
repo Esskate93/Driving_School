a6df1ea67ad4a150662e81da80fe55ad2ae68b65
Tio Gobin <tio.gobin@epitech.eu>
2019-06-13 17:55:38 +0200

    Little changes in routes

--- mysite/driving_school/views.py
+++ mysite/driving_school/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
-
+from driving_school.models import CustomUser
+import logging
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
@@ -12,3 +13,12 @@ class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+
+# from django.http import HttpResponse
+from django.views.generic import ListView
+from driving_school.models import Planning
+
+def get_users(request):
+    planning = Planning.objects.filter().order_by('date')
+    return render(request, 'student.html', {'planning': planning})
